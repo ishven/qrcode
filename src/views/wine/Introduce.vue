539949cc3484f5a798b99faf1b839eb42ba8c396
edplyp <edplyp@leemanpaper.com>
2023-05-16 16:31:35 +0800

    提交

--- src/views/wine/Introduce.vue
+++ src/views/wine/Introduce.vue
@@ -5,19 +5,29 @@
         <el-card class="box-card">
           <template #header>
             <div class="card-header">
-              <span style="font-weight: bold;">产品介绍</span>
+              <span style="font-weight: bold;">深圳市龙华区好神鹿鞭酒</span>
             </div>
           </template>
           <div class=" text">
-            <span style="font-weight: bold;">主要成分：</span>
+            <span style="font-weight: bold;">主要原料：</span>
           </div>
           <div class="text item">鹿茸.鹿鞭.鹿筋.鹿尾.人参等等……</div>
-          <div class="text">
-            <span style="font-weight: bold;">功效：</span>
+          <div class=" text">
+            <span style="font-weight: bold;">制作过程：</span>
           </div>
-          <div class="text item">滋阴壮肾阳、益精血，归肾、肝经，温能补肾阳，甘咸能滋养精血，可以用于辅助治疗肾阳不足、精血亏虚引起的阳痿滑精、宫冷不孕、身体虚弱、精神疲乏、畏寒、眩晕、耳鸣耳聋、腰脊冷痛等症。</div>
+          <div class="text item">将人参，鹿茸.鹿鞭鹿尾.鹿筋.放入瓶中，再将十年以上53度的酱香酒倒入密封两年以上</div>
+          <div class="text">
+            <span style="font-weight: bold;">功能功效：</span>
+          </div>
+          <div class="text item">鹿茸有壮元阳，补气血，益精髓，强筋骨。治虚劳羸瘦，精神倦乏，眩晕，耳聋，目暗，腰膝酸痛，阳痿，滑精，子宫虚冷，崩漏，带下的作用，鹿筋能强筋壮骨、养血通络、生精益髓，主治劳损，续绝伤，大壮筋骨，补阳气。
+            鹿鞭主治治阳痿，肾虚耳呜，妇人子宫寒冷，久不受孕，慢性睾丸发炎
+          </div>
+          <div class=" text">
+            <span style="font-weight: bold;">功能主治：</span>
+          </div>
+          <div class="text item">本药酒渐补下元，生精益血，壮阳健骨。 肝肾阴虚、肝阳上亢等阴虚阳盛体质之人</div>
           <div class="text ">
-            <span style="font-weight: bold;"> 用法与用量：</span>每晚10～20ml
+            <span style="font-weight: bold;"> 服用方法：</span>每晚睡前饮15-20ml。
           </div>
           <div class="text item">
             <span style="font-weight: bold;"> 零售价：</span>¥1988.00元
@@ -39,7 +49,7 @@
 }
 
 .box-card {
-  width: 480px;
+  width: 300px;
 }
 
 .card-header {
