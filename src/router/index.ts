32dd18c081aa7eb38fc9720ddf7fd3b8f46b7bad
ishven <2535418909@qq.com>
2023-05-19 19:26:22 +0800

    提交

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,12 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
   {
+    path: '/',
+    name: 'index',
+    title: '首页',
+    redirect: '/vue-qrcode'
+  },
+  {
     name: 'Introduce',
     path: '/vue-qrcode',
     component: () => import('@/views/wine/Introduce.vue'),
