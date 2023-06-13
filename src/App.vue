ecee23ea7b59298cb112d65acd3649640b1bf856
edplyp <edplyp@leemanpaper.com>
2023-06-13 14:24:32 +0800

    提交

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
-    <p>
-      <router-link to="/qrcode/wine/Introduce">1988</router-link>
-    </p>
-    <p>
-      <router-link to="/qrcode/wine/Introduce01">988</router-link>
-    </p>
+    <div v-show="env">
+      <p>
+        <router-link to="/qrcode/wine/Introduce">1988</router-link>
+      </p>
+      <p>
+        <router-link to="/qrcode/wine/Introduce01">988</router-link>
+      </p>
+    </div>
     <router-view></router-view>
   </div>
 </template>
 
 <script setup lang="ts">
+const env = import.meta.env.DEV
 </script>
 
 <style scoped></style>
