6e66dce8e1a8288c31e52829586eef988b27c1eb
edplyp <edplyp@leemanpaper.com>
2024-01-06 12:27:07 +0800

    提交

--- src/views/wine/Introduce03.vue
+++ src/views/wine/Introduce03.vue
@@ -19,7 +19,7 @@
 import { useRouter } from 'vue-router'
 const router = useRouter()
 function showDetail() {
-    router.replace({ name: "IntroduceDetail03" });
+    router.push({ name: "IntroduceDetail03" });
 }
 </script>
 
