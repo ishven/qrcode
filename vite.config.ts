9038ecc2165dba3875d7afc807afb1c480acee56
edplyp <edplyp@leemanpaper.com>
2024-05-31 13:49:23 +0800

    提交

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'docs' // 设置输出目录为docs，GitHub部署的目录是docs
+  },
   plugins: [vue()],
   base: 'qrcode',
   resolve: {
